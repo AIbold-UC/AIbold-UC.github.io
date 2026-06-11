6d08742db46d08d5e846cd121c1cbef0bb8247a6
Aibold <iboldam@mail.uc.edu>
2026-06-11 18:41:37 -0400

    General Requirements: resume page

---
+++ email.js
@@ -0,0 +1,14 @@
+
+var shown=false;
+function showhideEmail(){
+	if(shown){
+		document.getElementById('email').innerHTML = "Show my email";
+		shown=false;
+	}else{
+		var myemail="<a href='mailto:iboldam@mail.uc.edu'> iboldam@mail.uc.edu </a>";
+		document.getElementById('email').innerHTML = myemail;
+		shown=true;
+	}
+
+
+}
