a6d7ff41e976d30444707c69be09cdf37aad3f83
Aibold <iboldam@mail.uc.edu>
2026-06-11 19:02:24 -0400

    clocks + fixed issue with tags

---
+++ clocks.js
@@ -0,0 +1,19 @@
+function displayTime() {
+					document.getElementById('digit-clock').innerHTML="Current time: "+ new Date();
+				}
+				setInterval(displayTime,500);
+
+
+var canvas=document.getElementById("analog-clock");
+				var ctx= canvas.getContext( "2d")
+				var radius=canvas.height / 2;
+				ctx.translate(radius,radius);
+				radius=radius*0.9;
+
+				setInterval(drawClock,1000);
+				
+				function drawClock(){
+					drawFace(ctx,radius);
+					drawNumbers(ctx,radius);
+					drawTime(ctx,radius);
+				} 
