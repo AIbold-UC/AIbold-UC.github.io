d6a8ad2d67da56273ad9bf4b9700050e8a95ff05
Aibold <iboldam@mail.uc.edu>
2026-06-11 20:46:36 -0400

    attempt to get comic API up and running

--- API.js
+++ API.js
@@ -7,11 +7,28 @@ function jokeAPI(){
 							if (result.length==0) return;
 							
 							console.log("from jokeAPI: "+JSON.stringify(result));
-							$("#jokeResponse").text("a programming joke of the day: "+ result.joke);
+							$("#jokeResponse").text("a joke of the day: "+ result.joke);
 						})
 
 }
 $(function () {
     jokeAPI();                 //for initial setup
     setInterval(jokeAPI, 60000); 
+});
+
+//given that this was a provided api, and it seems to lead to the same comic each time, im gonna assume I don't need to randomize
+function comicAPI(){
+
+	$.get(https://xkcd.vercel.app/?comic=latest,
+						function (result) {
+							if (result.length==0) return;
+							
+							console.log("from comicAPI: "+JSON.stringify(result));
+							$("#comicText").text("a joke of the day: "+ result.alt);
+							$("#comicImage").attr("src", result.img);
+						})
+
+}
+$(function () {
+    comicAPI();                  
 });
