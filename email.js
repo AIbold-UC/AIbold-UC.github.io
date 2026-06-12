3388668733fbd06a6f12d1228c34f576d81797fb
Aibold <iboldam@mail.uc.edu>
2026-06-12 14:58:28 -0400

    (hopefully) final commit

--- email.js
+++ email.js
@@ -3,13 +3,13 @@ var shown=false;
 function showhideEmail(){
 	if(shown){
 		$("#email").html("Show my email");
-		//document.getElementById('email').innerHTML = "Show my email";
+
 		shown=false;
 	}else{
 		var myemail="<a href='mailto:iboldam@mail.uc.edu'> iboldam@mail.uc.edu </a>";
 		$("#email").html(myemail);
 
-		//document.getElementById('email').innerHTML = myemail;
+
 		shown=true;
 	}
 
