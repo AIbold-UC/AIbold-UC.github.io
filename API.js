8928f45ada46006e090b3f87ef9e8bdb376d6a5c
Aibold <iboldam@mail.uc.edu>
2026-06-11 20:07:07 -0400

    added jokeAPI

---
+++ API.js
@@ -0,0 +1,15 @@
+
+
+function jokeAPI(){
+
+	$.get("https://v2.jokeapi.dev/joke/Programming?type=single",
+						function (result) {
+							if (result.length==0) return;
+							
+							console.log("from jokeAPI: "+JSON.stringify(result));
+							$("#jokeResponse").text("a programming joke of the day: "+ result.joke);
+						})
+
+}
+
+setInterval(jokeAPI,60000)
